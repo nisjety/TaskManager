cbab1737333794c62538dd253881042f0e630b5e
ngtranp <phillip_tran@live.no>
2023-11-30 08:21:59 +0100

    Update IDataManagement.cs
    Check user ref

--- interfaces/IDataManagement.cs
+++ interfaces/IDataManagement.cs
@@ -1,6 +1,11 @@
 namespace TaskManager.interfaces;
 
-public interface IDataManagement
 {
-    
+    User GetUserByUsername(string username);
+    bool UsernameExists(string username);
+    bool EmailExists(string email);
+    void CreateUser(User user);
+    void CreateUserTask(Task task);
+    void DeleteUserTaskById(int taskId);
+    IEnumerable<Task> GetUserTasksForUser(int userId);
 }
